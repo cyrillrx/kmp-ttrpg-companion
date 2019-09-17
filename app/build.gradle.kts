d4708732bb2611b0863309c0cf3d09f8b7cac54b
Cyril Leroux <cyril0leroux@gmail.com>
2019-09-17 13:55:31 +0200

    Created a helper method to be able to use a 'release' bloc inside the 'buildTypes' bloc without using 'getByName("release")'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,16 +7,19 @@ plugins {
 android {
     compileSdkVersion(29)
     buildToolsVersion("29.0.2")
+
     defaultConfig {
-        applicationId = "com.cyrillrx.rpg"
         minSdkVersion(26)
         targetSdkVersion(29)
+        applicationId = "com.cyrillrx.rpg"
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -28,9 +31,19 @@ android {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8", "1.3.50"))
+
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("androidx.core:core-ktx:1.1.0")
+
     testImplementation("junit:junit:4.12")
+
     androidTestImplementation("androidx.test:runner:1.2.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
+
+// Helper methods
+
+@Throws(UnknownDomainObjectException::class)
+fun <T> NamedDomainObjectContainer<T>.release(configureAction: Action<in T>) {
+    getByName("release", configureAction)
 }
