9fea8eb14a280640159c51dc33e9384e60898225
Cyril Leroux <cyril0leroux@gmail.com>
2020-03-28 21:43:48 +0100

    Init Logger

--- app/src/main/java/com/cyrillrx/rpg/App.kt
+++ app/src/main/java/com/cyrillrx/rpg/App.kt
@@ -1,10 +1,22 @@
 package com.cyrillrx.rpg
 
 import android.app.Application
+import com.cyrillrx.logger.Logger
+import com.cyrillrx.logger.Severity
+import com.cyrillrx.logger.extension.LogCat
 
 /**
  * @author Cyril Leroux
  *         Created on 24/09/2019.
  */
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Logger.initialize()
+        if (BuildConfig.DEBUG) {
+            Logger.addChild(LogCat(Severity.VERBOSE))
+        }
+    }
 }
