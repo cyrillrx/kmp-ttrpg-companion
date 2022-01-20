63e6bea9f5274ff845dac7b5d01cbf9b5e6f26cd
Cyril Leroux <cyril0leroux@gmail.com>
2022-01-21 00:48:27 +0100

    [Lib] Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,17 +4,17 @@ plugins {
 }
 
 android {
-    compileSdkVersion(Version.compileSdk)
-    buildToolsVersion(Version.buildToolsVersion)
+    compileSdk = Version.compileSdk
 
     defaultConfig {
-        minSdkVersion(Version.minSdk)
-        targetSdkVersion(Version.targetSdk)
-        applicationId("com.cyrillrx.rpg")
-        versionCode(1)
-        versionName("1.0")
+        minSdk = Version.minSdk
+        targetSdk = Version.targetSdk
 
-        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+        applicationId = "com.cyrillrx.rpg"
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     kotlinOptions {
@@ -53,20 +53,19 @@ dependencies {
     implementation(files("$rootDir/libs/core.2021.09.14.aar"))
     implementation(files("$rootDir/libs/templates.2021.09.14.aar"))
 
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
-
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 
     implementation("androidx.compose.ui:ui:${Version.compose}")
-    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
     implementation("androidx.compose.material:material:${Version.compose}")
     implementation("androidx.compose.ui:ui-tooling:${Version.compose}")
 
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.code.gson:gson:2.8.8")
 
     testImplementation("junit:junit:4.13.2")
 
--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,12 +1,11 @@
 object Version {
-    const val kotlin = "1.5.21"
+    const val kotlin = "1.6.10"
 
-    const val compileSdk = 30
-    const val buildToolsVersion = "30.0.2"
+    const val compileSdk = 31
 
     const val minSdk = 24
-    const val targetSdk = 30
+    const val targetSdk = 31
 
-    const val java = "1.8"
-    const val compose = "1.0.2"
+    const val java = "11"
+    const val compose = "1.0.5"
 }
