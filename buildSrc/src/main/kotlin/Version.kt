6049388cc7c68f71c2e98ffa910fd57fe3bc87f2
Cyril Leroux <cyril0leroux@gmail.com>
2024-11-27 23:34:30 +0100

    ComposeApp: Bump compile SDK from 34 to 35

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Version {
     const val MIN_SDK = 24
-    const val COMPILE_SDK = 34
+    const val COMPILE_SDK = 35
 
     val java = JavaVersion.VERSION_17
     const val JVM_TARGET = "17"
