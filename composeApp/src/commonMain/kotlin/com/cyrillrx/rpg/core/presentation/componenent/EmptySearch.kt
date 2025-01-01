7c9922f8679cb18703d4ab9f63bc8122176fbd6a
Cyril Leroux <cyril0leroux@gmail.com>
2025-01-01 11:58:48 +0100

    ComposeApp: Update EmptySearch.kt component

--- composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/presentation/componenent/EmptySearch.kt
+++ composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/presentation/componenent/EmptySearch.kt
@@ -7,11 +7,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import com.cyrillrx.rpg.core.presentation.theme.spacingCommon
+import org.jetbrains.compose.resources.stringResource
+import rpg_companion.composeapp.generated.resources.Res
+import rpg_companion.composeapp.generated.resources.no_result_found
+import rpg_companion.composeapp.generated.resources.no_result_found_for_query
 
 @Composable
 fun EmptySearch(searchQuery: String) {
+    val text = if (searchQuery.isBlank()) {
+        stringResource(Res.string.no_result_found)
+    } else {
+        stringResource(Res.string.no_result_found_for_query, searchQuery)
+    }
     Text(
-        text = "No results found for '$searchQuery'",
+        text = text,
         textAlign = TextAlign.Center,
         modifier = Modifier
             .fillMaxSize()
