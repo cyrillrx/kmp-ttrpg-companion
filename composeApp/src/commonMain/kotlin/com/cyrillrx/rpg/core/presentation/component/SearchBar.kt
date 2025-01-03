e144178f7534a70127d1bb5afef3dea7d3bb77cc
Cyril Leroux <cyril0leroux@gmail.com>
2025-01-04 00:06:30 +0100

    ComposeApp: Simplify SearchBar

--- composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/presentation/component/SearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/presentation/component/SearchBar.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.selection.LocalTextSelectionColors
-import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
@@ -21,7 +19,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
@@ -47,43 +44,36 @@ fun SearchBar(
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
 
-    CompositionLocalProvider(
-        LocalTextSelectionColors provides TextSelectionColors(
-            handleColor = Purple700,
-            backgroundColor = Purple700,
+    OutlinedTextField(
+        value = query,
+        onValueChange = onQueryChanged,
+        shape = RoundedCornerShape(50),
+        colors = TextFieldDefaults.colors(),
+        placeholder = { Text(text = hint) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+            )
+        },
+        singleLine = true,
+        keyboardActions = KeyboardActions(onSearch = { keyboardController?.hide() }),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Search,
         ),
-    ) {
-        OutlinedTextField(
-            value = query,
-            onValueChange = onQueryChanged,
-            shape = RoundedCornerShape(50),
-            colors = TextFieldDefaults.colors(),
-            placeholder = { Text(text = hint) },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                )
-            },
-            singleLine = true,
-            keyboardActions = KeyboardActions(onSearch = { keyboardController?.hide() }),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Search,
-            ),
-            trailingIcon = {
-                AnimatedVisibility(visible = query.isNotBlank()) {
-                    IconButton(onClick = { onQueryChanged("") }) {
-                        Icon(
-                            imageVector = Icons.Default.Close,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.onSurface,
-                        )
-                    }
+        trailingIcon = {
+            AnimatedVisibility(visible = query.isNotBlank()) {
+                IconButton(onClick = { onQueryChanged("") }) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
                 }
-            },
-            modifier = modifier,
-        )
-    }
+            }
+        },
+        modifier = modifier,
+    )
 }
