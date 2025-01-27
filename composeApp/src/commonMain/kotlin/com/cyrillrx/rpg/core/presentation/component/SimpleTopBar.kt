f897531027595c0823cc2da4062aa4ab5a59378c
Cyril Leroux <cyril0leroux@gmail.com>
2025-01-28 00:12:53 +0100

    ComposeApp: Fix SimpleTopBar background color

--- composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/presentation/component/SimpleTopBar.kt
+++ composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/presentation/component/SimpleTopBar.kt
@@ -5,8 +5,10 @@ import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
@@ -34,5 +36,6 @@ fun SimpleTopBar(
                 )
             }
         },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background),
     )
 }
