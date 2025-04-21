2f06c7c21f404c10351974a2672f87e849e53990
Cyril Leroux <cyril0leroux@gmail.com>
2025-04-21 22:35:09 +0200

    Server: Remove campaignId from PlayerCharacter

--- spring-server/src/main/kotlin/com/cyrillrx/rpg/server/character/data/RamPlayerCharacterRepository.kt
+++ spring-server/src/main/kotlin/com/cyrillrx/rpg/server/character/data/RamPlayerCharacterRepository.kt
@@ -8,21 +8,38 @@ import org.springframework.stereotype.Repository
 class RamPlayerCharacterRepository : PlayerCharacterRepository {
 
     private val characters = mutableListOf<PlayerCharacter>().apply {
-        add(PlayerCharacter("1", "Aragorn", "1", mapOf(
-            "class" to "Ranger",
-            "level" to 10,
-            "race" to "Human"
-        )))
-        add(PlayerCharacter("2", "Gandalf", "1", mapOf(
-            "class" to "Wizard",
-            "level" to 20,
-            "race" to "Maiar"
-        )))
-        add(PlayerCharacter("3", "V", "3", mapOf(
-            "clan" to "Toreador",
-            "generation" to 10,
-            "humanity" to 7
-        )))
+        val character1 = PlayerCharacter(
+            "1",
+            "Aragorn",
+            mapOf(
+                "class" to "Ranger",
+                "level" to 10,
+                "race" to "Human"
+            )
+        )
+        add(character1)
+
+        val character2 = PlayerCharacter(
+            "2",
+            "Gandalf",
+            mapOf(
+                "class" to "Wizard",
+                "level" to 20,
+                "race" to "Maiar",
+            ),
+        )
+        add(character2)
+
+        val character3 = PlayerCharacter(
+            "3",
+            "V",
+            mapOf(
+                "clan" to "Toreador",
+                "generation" to 10,
+                "humanity" to 7,
+            ),
+        )
+        add(character3)
     }
 
     override suspend fun getAll(): List<PlayerCharacter> = characters
--- spring-server/src/main/kotlin/com/cyrillrx/rpg/server/character/domain/PlayerCharacter.kt
+++ spring-server/src/main/kotlin/com/cyrillrx/rpg/server/character/domain/PlayerCharacter.kt
@@ -3,6 +3,5 @@ package com.cyrillrx.rpg.server.character.domain
 data class PlayerCharacter(
     val id: String,
     val name: String,
-    val campaignId: String,
     val attributes: Map<String, Any> = emptyMap(),
 )
