788259c175d97310353df25d6cc87143e1fd54dc
Cyril Leroux <cyril0leroux@gmail.com>
2026-03-10 09:02:31 +0100

    Shared: FileReaderError class becomes sealed

--- cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/core/domain/Error.kt
+++ cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/core/domain/Error.kt
@@ -2,7 +2,7 @@ package com.cyrillrx.core.domain
 
 sealed interface Error
 
-open class FileReaderError(val fileName: String, val reason: String?) : Error {
+sealed class FileReaderError(val fileName: String, val reason: String?) : Error {
     class Unknown(fileName: String, reason: String? = null) : FileReaderError(fileName, reason)
     class FileNotFound(fileName: String, reason: String? = null) : FileReaderError(fileName, reason)
     class UnableToRead(fileName: String, reason: String? = null) : FileReaderError(fileName, reason)
