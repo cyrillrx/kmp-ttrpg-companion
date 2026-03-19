adabeeae413cfe7b0784607a5b55dc11b2b494cc
Cyril Leroux <cyril0leroux@gmail.com>
2026-03-19 01:09:50 +0100

    feat(home): replace reference buttons with a CompendiumButton row
    Also, remove access to AlternativeSpellBook (displaying a carousel of cards)

--- cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/home/presentation/HomeScreen.kt
+++ cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/home/presentation/HomeScreen.kt
@@ -1,9 +1,15 @@
 package com.cyrillrx.rpg.home.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AutoAwesome
+import androidx.compose.material.icons.filled.Diamond
+import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,13 +17,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.cyrillrx.rpg.core.presentation.theme.AppThemePreview
+import com.cyrillrx.rpg.core.presentation.theme.spacingCommon
 import com.cyrillrx.rpg.core.presentation.theme.spacingLarge
 import com.cyrillrx.rpg.home.presentation.navigation.HomeRouter
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import rpg_companion.composeapp.generated.resources.Res
 import rpg_companion.composeapp.generated.resources.app_name
-import rpg_companion.composeapp.generated.resources.btn_alternative_spell_book
 import rpg_companion.composeapp.generated.resources.btn_bestiary
 import rpg_companion.composeapp.generated.resources.btn_campaign_list
 import rpg_companion.composeapp.generated.resources.btn_character_sheets
@@ -43,12 +49,37 @@ fun HomeScreen(router: HomeRouter) {
             HomeButton(stringResource(Res.string.btn_campaign_list), router::openCampaignList)
             HomeButton(stringResource(Res.string.btn_character_sheets), router::openCharacterSheets)
 
-            HorizontalDivider()
+            Text(
+                text = "Compendium",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = spacingCommon, vertical = spacingCommon),
+            )
 
-            HomeButton(stringResource(Res.string.btn_spell_book), router::openSpellBook)
-            HomeButton(stringResource(Res.string.btn_alternative_spell_book), router::openAlternativeSpellBook)
-            HomeButton(stringResource(Res.string.btn_bestiary), router::openBestiary)
-            HomeButton(stringResource(Res.string.btn_inventory), router::openMagicalItems)
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(spacingCommon),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacingCommon),
+            ) {
+                CompendiumButton(
+                    label = stringResource(Res.string.btn_spell_book),
+                    icon = Icons.Filled.AutoAwesome,
+                    onClick = router::openSpellBook,
+                    modifier = Modifier.weight(1f),
+                )
+                CompendiumButton(
+                    label = stringResource(Res.string.btn_inventory),
+                    icon = Icons.Filled.Diamond,
+                    onClick = router::openMagicalItems,
+                    modifier = Modifier.weight(1f),
+                )
+                CompendiumButton(
+                    label = stringResource(Res.string.btn_bestiary),
+                    icon = Icons.Filled.Pets,
+                    onClick = router::openBestiary,
+                    modifier = Modifier.weight(1f),
+                )
+            }
         }
     }
 }
