98a47fccf11095cbf091dd3432b88ac6a292b856
Cyril Leroux <cyril0leroux@gmail.com>
2026-03-23 23:21:43 +0100

    feat(magicalitem): add compact list screen with type-colored items

--- cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/home/presentation/navigation/HomeRouter.kt
+++ cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/home/presentation/navigation/HomeRouter.kt
@@ -35,7 +35,7 @@ class HomeRouterImpl(private val navController: NavController) : HomeRouter {
     }
 
     override fun openMagicalItems() {
-        navController.navigate(MagicalItemRoute.CardCarousel)
+        navController.navigate(MagicalItemRoute.List)
     }
 
     override fun openBestiary() {
