b0cf990d99ec019ee7aa0084f7989b0e6f4595f6
Cyril Leroux <cyril0leroux@gmail.com>
2026-03-28 10:40:46 +0100

    feat(userlist): add UserList SQLDelight schema and Database methods

--- cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/rpg/core/data/cache/Database.kt
+++ cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/rpg/core/data/cache/Database.kt
@@ -3,6 +3,7 @@ package com.cyrillrx.rpg.core.data.cache
 import com.cyrillrx.rpg.cache.AppDatabase
 import com.cyrillrx.rpg.campaign.domain.Campaign
 import com.cyrillrx.rpg.campaign.domain.RuleSet
+import com.cyrillrx.rpg.userlist.domain.UserList
 
 internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
     private val database = AppDatabase(databaseDriverFactory.createDriver())
@@ -31,7 +32,42 @@ internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
     private fun mapCampaignSelecting(id: String, name: String, ruleSet: Long): Campaign =
         Campaign(id = id, name = name, ruleSet = RuleSet.fromInt(ruleSet.toInt()))
 
+    fun getAllUserLists(type: UserList.Type): List<UserList> =
+        dbQuery.selectAllUserListsByType(type.name, ::mapUserListSelecting).executeAsList()
+
+    fun getUserList(id: String): UserList? = dbQuery.selectUserListById(id, ::mapUserListSelecting).executeAsOneOrNull()
+
+    fun insertUserList(list: UserList) {
+        dbQuery.insertUserList(
+            id = list.id,
+            name = list.name,
+            type = list.type.name,
+            itemIds = list.itemIds.joinToString(LIST_DELIMITER),
+        )
+    }
+
+    fun updateUserList(list: UserList) {
+        dbQuery.updateUserList(
+            name = list.name,
+            itemIds = list.itemIds.joinToString(LIST_DELIMITER),
+            id = list.id,
+        )
+    }
+
+    fun deleteUserList(id: String) {
+        dbQuery.deleteUserList(id)
+    }
+
+    private fun mapUserListSelecting(id: String, name: String, type: String, itemIds: String): UserList =
+        UserList(
+            id = id,
+            name = name,
+            type = UserList.Type.valueOf(type),
+            itemIds = if (itemIds.isEmpty()) emptyList() else itemIds.split(LIST_DELIMITER),
+        )
+
     companion object {
         const val DATABASE_NAME = "ttrpg_companion.db"
+        const val LIST_DELIMITER = "|"
     }
 }
