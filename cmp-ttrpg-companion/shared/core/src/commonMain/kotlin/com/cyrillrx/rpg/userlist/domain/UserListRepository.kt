e3fc9d8bae2fe37f6786086e8c1e76febb8fee77
Cyril Leroux <cyril0leroux@gmail.com>
2026-03-28 11:09:03 +0100

    style(UserListRepository): remove empty lines

--- cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/rpg/userlist/domain/UserListRepository.kt
+++ cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/rpg/userlist/domain/UserListRepository.kt
@@ -2,10 +2,7 @@ package com.cyrillrx.rpg.userlist.domain
 
 interface UserListRepository {
     suspend fun getAll(type: UserList.Type): List<UserList>
-
     suspend fun get(id: String): UserList?
-
     suspend fun save(list: UserList)
-
     suspend fun delete(id: String)
 }
