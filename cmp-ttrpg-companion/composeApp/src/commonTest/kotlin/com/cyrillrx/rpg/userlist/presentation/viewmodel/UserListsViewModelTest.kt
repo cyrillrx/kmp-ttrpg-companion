e20adb3a90d1796ec4944a9b2310476e0eb167ea
Cyril Leroux <cyril0leroux@gmail.com>
2026-03-29 16:54:55 +0200

    test(userlist): add Loading and Error state tests to UserListsViewModelTest

--- cmp-ttrpg-companion/composeApp/src/commonTest/kotlin/com/cyrillrx/rpg/userlist/presentation/viewmodel/UserListsViewModelTest.kt
+++ cmp-ttrpg-companion/composeApp/src/commonTest/kotlin/com/cyrillrx/rpg/userlist/presentation/viewmodel/UserListsViewModelTest.kt
@@ -3,6 +3,7 @@ package com.cyrillrx.rpg.userlist.presentation.viewmodel
 import com.cyrillrx.rpg.spell.presentation.viewmodel.UserListsViewModel
 import com.cyrillrx.rpg.userlist.data.RamUserListRepository
 import com.cyrillrx.rpg.userlist.domain.UserList
+import com.cyrillrx.rpg.userlist.domain.UserListRepository
 import com.cyrillrx.rpg.userlist.presentation.UserListsState
 import com.cyrillrx.rpg.userlist.presentation.navigation.UserListRouter
 import kotlinx.coroutines.Dispatchers
@@ -39,6 +40,26 @@ class UserListsViewModelTest {
     }
 
     @Test
+    fun `initial state is Loading before coroutines run`() = runTest(testDispatcher) {
+        val viewModel = UserListsViewModel(UserList.Type.SPELL, router, repository)
+
+        assertIs<UserListsState.Body.Loading>(viewModel.state.value.body)
+    }
+
+    @Test
+    fun `state is Error when repository throws`() = runTest(testDispatcher) {
+        val viewModel = UserListsViewModel(UserList.Type.SPELL, router, FailingUserListRepository())
+
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+            viewModel.state.collect {}
+        }
+
+        advanceUntilIdle()
+
+        assertIs<UserListsState.Body.Error>(viewModel.state.value.body)
+    }
+
+    @Test
     fun `initial state is Empty when no lists exist`() = runTest(testDispatcher) {
         val viewModel = UserListsViewModel(UserList.Type.SPELL, router, repository)
 
@@ -60,9 +81,7 @@ class UserListsViewModelTest {
         }
 
         advanceUntilIdle()
-
         viewModel.createList("Fighting spells")
-
         advanceUntilIdle()
 
         val body = assertIs<UserListsState.Body.WithData>(viewModel.state.value.body)
@@ -80,7 +99,6 @@ class UserListsViewModelTest {
         }
 
         advanceUntilIdle()
-
         viewModel.createList("Fighting spells")
         advanceUntilIdle()
 
@@ -103,7 +121,6 @@ class UserListsViewModelTest {
         }
 
         advanceUntilIdle()
-
         viewModel.createList("Spellbook")
         advanceUntilIdle()
 
@@ -118,6 +135,13 @@ class UserListsViewModelTest {
 
 private class NoOpUserListRouter : UserListRouter {
     override fun navigateUp() = Unit
+}
+
+private class FailingUserListRepository : UserListRepository {
+    override suspend fun getAll(type: UserList.Type): List<UserList> = error("Repository failure")
+    override suspend fun get(id: String): UserList? = null
+    override suspend fun save(list: UserList) = Unit
+    override suspend fun delete(id: String) = Unit
 }
 
 private class TrackingUserListRouter : UserListRouter {
