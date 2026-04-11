a20e91a6d811000d4cb8908c7e3369e32fb6b632
Cyril Leroux <cyril0leroux@gmail.com>
2026-04-12 00:27:59 +0200

    build(core): migrate to com.android.kotlin.multiplatform.library plugin
    Replace deprecated com.android.library with the AGP 9.0-compatible
    com.android.kotlin.multiplatform.library plugin in shared/core; move
    Android config into kotlin { android {} } and remove the top-level
    android {} block.

--- cmp-ttrpg-companion/shared/core/build.gradle.kts
+++ cmp-ttrpg-companion/shared/core/build.gradle.kts
@@ -3,20 +3,19 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.ktlint)
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.sqldelight)
     alias(libs.plugins.kover)
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            compileTaskProvider.configure {
-                compilerOptions {
-                    jvmTarget.set(JvmTarget.JVM_17)
-                }
-            }
+    android {
+        namespace = "com.cyrillrx.rpg.core"
+        compileSdk = Version.COMPILE_SDK
+        minSdk = Version.MIN_SDK
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 
@@ -54,19 +53,6 @@ kotlin {
         jvmMain.dependencies {
             implementation(libs.sqldelight.jvm)
         }
-    }
-}
-
-android {
-    namespace = "com.cyrillrx.rpg.core"
-    compileSdk = Version.COMPILE_SDK
-
-    defaultConfig {
-        minSdk = Version.MIN_SDK
-    }
-    compileOptions {
-        sourceCompatibility = Version.java
-        targetCompatibility = Version.java
     }
 }
 
