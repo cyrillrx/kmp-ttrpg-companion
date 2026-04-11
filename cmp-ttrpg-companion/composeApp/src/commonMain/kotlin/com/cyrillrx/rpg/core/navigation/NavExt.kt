8815906a2d26318b70657b1dbf795dcf3802443a
Cyril Leroux <cyril0leroux@gmail.com>
2026-04-11 13:59:08 +0200

    refactor(nav): simplify navigateUp to delegate to removeLastOrNull

--- cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/navigation/NavExt.kt
+++ cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/core/navigation/NavExt.kt
@@ -4,5 +4,5 @@ import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 
 fun NavBackStack<NavKey>.navigateUp() {
-    if (size > 1) removeAt(index = size - 1)
+    removeLastOrNull()
 }
