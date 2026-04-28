de85d363f26d0621d2f593c70e64b78c31f22262
Cyril Leroux <cyril0leroux@gmail.com>
2026-04-28 21:17:49 +0200

    feat(core): add partitionBy() and rewrite partition() on top of it

--- cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/core/domain/Result.kt
+++ cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/core/domain/Result.kt
@@ -5,14 +5,19 @@ sealed interface Result<out Data, out E : Error> {
     data class Failure<out E : Error>(val error: E) : Result<Nothing, E>
 }
 
-fun <Data, E : Error> List<Result<Data, E>>.partition(): Pair<List<Data>, List<E>> {
+fun <T, Data, E : Error> List<T>.partitionBy(
+    transform: (T) -> Result<Data, E>,
+): Pair<List<Data>, List<E>> {
     val successes = mutableListOf<Data>()
     val failures = mutableListOf<E>()
-    for (result in this) {
-        when (result) {
+    for (item in this) {
+        when (val result = transform(item)) {
             is Result.Success -> successes.add(result.value)
             is Result.Failure -> failures.add(result.error)
         }
     }
     return successes to failures
 }
+
+fun <Data, E : Error> List<Result<Data, E>>.partition(): Pair<List<Data>, List<E>> =
+    partitionBy { it }
