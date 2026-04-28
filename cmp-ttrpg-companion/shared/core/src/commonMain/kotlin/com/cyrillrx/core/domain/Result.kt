7de2276ac15db42d0c412f34fdc17a3cea7f708f
Cyril Leroux <cyril0leroux@gmail.com>
2026-04-28 20:48:11 +0200

    feat(core): add partition() extension on List<Result<Data, E>>

--- cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/core/domain/Result.kt
+++ cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/core/domain/Result.kt
@@ -4,3 +4,15 @@ sealed interface Result<out Data, out E : Error> {
     data class Success<out Data>(val value: Data) : Result<Data, Nothing>
     data class Failure<out E : Error>(val error: E) : Result<Nothing, E>
 }
+
+fun <Data, E : Error> List<Result<Data, E>>.partition(): Pair<List<Data>, List<E>> {
+    val successes = mutableListOf<Data>()
+    val failures = mutableListOf<E>()
+    for (result in this) {
+        when (result) {
+            is Result.Success -> successes.add(result.value)
+            is Result.Failure -> failures.add(result.error)
+        }
+    }
+    return successes to failures
+}
