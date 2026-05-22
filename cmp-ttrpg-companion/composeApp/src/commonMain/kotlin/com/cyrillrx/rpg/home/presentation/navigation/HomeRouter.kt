44b273fe5aba881510be4f4fb1d4c0db87c7fc6d
Cyril Leroux <cyril0leroux@gmail.com>
2026-05-22 09:12:08 +0200

    feat(home): add openSettings navigation to HomeRouter

--- cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/home/presentation/navigation/HomeRouter.kt
+++ cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/home/presentation/navigation/HomeRouter.kt
@@ -6,6 +6,7 @@ import com.cyrillrx.rpg.campaign.navigation.CampaignRoute
 import com.cyrillrx.rpg.character.presentation.navigation.CharacterRoute
 import com.cyrillrx.rpg.creature.presentation.navigation.MonsterRoute
 import com.cyrillrx.rpg.magicalitem.presentation.navigation.MagicalItemRoute
+import com.cyrillrx.rpg.settings.presentation.navigation.SettingsRoute
 import com.cyrillrx.rpg.spell.presentation.navigation.SpellRoute
 import com.cyrillrx.rpg.userlist.presentation.navigation.UserListRoute
 
@@ -18,6 +19,7 @@ interface HomeRouter {
     fun openMySpellLists() {}
     fun openMyMagicalItemLists() {}
     fun openMyMonsterLists() {}
+    fun openSettings() {}
 }
 
 class HomeRouterImpl(private val backStack: NavBackStack<NavKey>) : HomeRouter {
@@ -51,5 +53,9 @@ class HomeRouterImpl(private val backStack: NavBackStack<NavKey>) : HomeRouter {
 
     override fun openMyMonsterLists() {
         backStack.add(UserListRoute.Creature)
+    }
+
+    override fun openSettings() {
+        backStack.add(SettingsRoute.Main)
     }
 }
