dfea754f1f79dc82ecdfc14cb12e28e75420e629
Cyril Leroux <cyril0leroux@gmail.com>
2026-05-22 09:12:01 +0200

    feat(settings): add SettingsRoute and SettingsRouter

---
+++ cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/settings/presentation/navigation/SettingsRoute.kt
@@ -0,0 +1,27 @@
+package com.cyrillrx.rpg.settings.presentation.navigation
+
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import com.cyrillrx.rpg.settings.domain.UserPreferencesRepository
+import com.cyrillrx.rpg.settings.presentation.SettingsScreen
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.modules.PolymorphicModuleBuilder
+
+sealed interface SettingsRoute {
+    @Serializable
+    data object Main : NavKey
+}
+
+fun PolymorphicModuleBuilder<NavKey>.registerSettingsRoutes() {
+    subclass(SettingsRoute.Main::class, SettingsRoute.Main.serializer())
+}
+
+fun EntryProviderScope<NavKey>.handleSettingsRoutes(
+    backStack: NavBackStack<NavKey>,
+    prefsRepository: UserPreferencesRepository,
+) {
+    entry<SettingsRoute.Main> {
+        SettingsScreen(router = SettingsRouterImpl(backStack))
+    }
+}
