37e113a57006a76ed12eccdda1ae3559ee461c15
Cyril Leroux <cyril0leroux@gmail.com>
2026-05-26 09:04:31 +0200

    feat(character): add SwipeToDelete logic to CharacterListViewModel

--- cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/character/presentation/viewmodel/CharacterListViewModelFactory.kt
+++ cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/character/presentation/viewmodel/CharacterListViewModelFactory.kt
@@ -5,14 +5,18 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.cyrillrx.rpg.character.domain.CharacterRepository
 import com.cyrillrx.rpg.character.presentation.navigation.CharacterRouter
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlin.reflect.KClass
 
 class CharacterListViewModelFactory(
     private val router: CharacterRouter,
     private val repository: CharacterRepository,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: KClass<T>, extras: CreationExtras): T {
-        return CharacterListViewModel(router, repository) as T
+        return CharacterListViewModel(router, repository, ioDispatcher) as T
     }
 }
