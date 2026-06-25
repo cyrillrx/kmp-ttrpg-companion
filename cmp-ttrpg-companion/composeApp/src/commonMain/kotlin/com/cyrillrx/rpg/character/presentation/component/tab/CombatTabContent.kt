55fbd8cb0bf2621a5a1b62e1b39a428164f481ea
Cyril Leroux <cyril0leroux@gmail.com>
2026-06-25 08:55:32 +0200

    feat(character): add the combat section divider to the combat tab

--- cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/character/presentation/component/tab/CombatTabContent.kt
+++ cmp-ttrpg-companion/composeApp/src/commonMain/kotlin/com/cyrillrx/rpg/character/presentation/component/tab/CombatTabContent.kt
@@ -4,13 +4,19 @@ import androidx.compose.runtime.Composable
 import com.cyrillrx.rpg.character.presentation.CharacterEditState
 import com.cyrillrx.rpg.character.presentation.CharacterEditState.Loaded.EditingField
 import com.cyrillrx.rpg.character.presentation.component.section.CombatRow
+import com.cyrillrx.rpg.character.presentation.component.section.SheetDivider
 import com.cyrillrx.rpg.character.presentation.component.section.WalkSpeedRow
+import org.jetbrains.compose.resources.stringResource
+import rpg_companion.composeapp.generated.resources.Res
+import rpg_companion.composeapp.generated.resources.label_combat
 
 @Composable
 internal fun CombatTabContent(
     state: CharacterEditState.Loaded,
     onFieldTapped: (EditingField) -> Unit,
 ) {
+    SheetDivider(stringResource(Res.string.label_combat))
+
     CombatRow(
         armorClass = state.character.armorClass,
         initiative = state.character.initiativeModifier(),
