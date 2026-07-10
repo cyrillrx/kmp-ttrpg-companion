c44e30900a39de2ca2437c602aec82a456960764
Cyril Leroux <cyril0leroux@gmail.com>
2026-07-10 15:49:05 +0200

    refactor(data): run SQLDelight repository access on an injected IO dispatcher

--- cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/rpg/userlist/data/SQLDelightUserListRepository.kt
+++ cmp-ttrpg-companion/shared/core/src/commonMain/kotlin/com/cyrillrx/rpg/userlist/data/SQLDelightUserListRepository.kt
@@ -4,19 +4,27 @@ import com.cyrillrx.rpg.core.data.cache.Database
 import com.cyrillrx.rpg.core.data.cache.DatabaseDriverFactory
 import com.cyrillrx.rpg.userlist.domain.UserList
 import com.cyrillrx.rpg.userlist.domain.UserListRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
 
-class SQLDelightUserListRepository(databaseDriverFactory: DatabaseDriverFactory) : UserListRepository {
+class SQLDelightUserListRepository(
+    databaseDriverFactory: DatabaseDriverFactory,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : UserListRepository {
     private val database = Database(databaseDriverFactory)
 
-    override suspend fun getAll(type: UserList.Type): List<UserList> = database.getAllUserLists(type)
+    override suspend fun getAll(type: UserList.Type): List<UserList> =
+        withContext(ioDispatcher) { database.getAllUserLists(type) }
 
-    override suspend fun get(id: String): UserList? = database.getUserList(id)
+    override suspend fun get(id: String): UserList? = withContext(ioDispatcher) { database.getUserList(id) }
 
     override suspend fun save(list: UserList) {
-        database.saveUserList(list)
+        withContext(ioDispatcher) { database.saveUserList(list) }
     }
 
     override suspend fun delete(id: String) {
-        database.deleteUserList(id)
+        withContext(ioDispatcher) { database.deleteUserList(id) }
     }
 }
